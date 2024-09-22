7f54681d4e8d9a4bb3cf1a95d8840ebf580ce97e
orirribas2000@gmail.com <orirribas2000@gmail.com>
2024-09-22 23:52:07 +0200

    Practica a entregar

--- 00 Processos/Exercici0/src/main/java/com/project/Task.java
+++ 00 Processos/Exercici0/src/main/java/com/project/Task.java
@@ -1,19 +1,4 @@
-package main.java.com.project;
+package com.project;
 
-public class Task extends Thread implements Runnable{
-
-    private String info;
-
-    public Task(String info) {
-        this.info = info;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("copiando archivos");
-    }
+public class Task {
 }
---
+++ 00 Processos/Exercici1/src/main/java/com/project/TaskEscriure.java
@@ -0,0 +1,8 @@
+package com.project;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Task {
+    private ConcurrentHashMap<s>
+
+}
